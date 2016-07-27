2469076d14ff989a8df893cae48b44bf1c9d1996
Grigory Alexandrov <alexaandrov@gmail.com>
2016-07-27 21:24:54 +0300

    + not worked Config

--- app/Config.php
+++ app/Config.php
@@ -1,14 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: greg
- * Date: 27.07.2016
- * Time: 6:15
- */
 
 namespace App;
 
 class Config
 {
     use Singleton;
+
+    protected $configFile;
+
+    protected $data;
+
+    protected function __construct()
+    {
+        $this->configFile = __DIR__ . '/../config.php';
+        $data = require $this->configFile;
+        var_dump($data);
+    }
+
+    public function isNew()
+    {
+        return empty($this->id);
+    }
+
+    public function insert()
+    {
+        if (! $this->isNew()) {
+            return;
+        }
+        $columns = [];
+        $values = [];
+        foreach ($this as $key => $value) {
+            if ('id' == $key) {
+                continue;
+            }
+            $columns[] = $key;
+            $values[':'.$key] = $value;
+        }
+        $sql = 'INSERT INTO ' . static::TABLE .
+            ' (' . implode(', ', $columns) . ')
+            VALUES (' . implode(', ', array_keys($values)) . ');';
+
+        $db = Db::instance();
+        $db->execute($sql, $values);
+    }
 }
