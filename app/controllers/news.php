97a38b3914c94b0eb1ef120e61053187b43a10ea
Grigory Alexandrov <alexaandrov@gmail.com>
2016-07-28 09:31:05 +0300

    + full controller for news

--- app/controllers/news.php
+++ app/controllers/news.php
@@ -1,16 +1,29 @@
 <?php
 
-require __DIR__ . '/../../autoload.php';
+namespace App\Controllers;
 
 use App\View;
 use App\Models\News;
 
-$view = new View();
-$view->title = 'News';
-$view->news = News::FindAll();
+class News
+{
+    protected $view;
 
-if (! empty($_GET['id'])) {
-    $view->news = News::findById((int)$_GET['id']);
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
+    public function action($action)
+    {
+        $methodName = 'action' . $action;
+        return $this->$methodName();
+    }
+
+    protected function actionIndex()
+    {
+        $this->view->title = 'News';
+        $this->view->news = News::FindAll();
+        $this->view->display(__DIR__ . '/../views/news.php');
+    }
 }
-
-$view->display(__DIR__ . '/../views/news.php');
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
-header("Location: " .'/app/controllers/index.php');
+//header("Location: " .'/app/controllers/index.php');
+
+require __DIR__ . '/autoload.php';
+
+$controller = new \App\Controllers\News();
+$controller->action('Index');
