cc0e0310528e5caac6b95a91cf61be8a5f4d69ce
Grigory Alexandrov <alexaandrov@gmail.com>
2016-07-28 09:35:07 +0300

    Up News controller

--- app/controllers/News.php
+++ app/controllers/News.php
@@ -3,7 +3,6 @@
 namespace App\Controllers;
 
 use App\View;
-use App\Models\News;
 
 class News
 {
@@ -23,7 +22,8 @@ class News
     protected function actionIndex()
     {
         $this->view->title = 'News';
-        $this->view->news = News::FindAll();
+        $this->view->news = \App\Models\News::FindAll();
         $this->view->display(__DIR__ . '/../views/news.php');
     }
+
 }
