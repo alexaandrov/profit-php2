2b20df2e29d99c4b66f1a7a3607a8b05642452c3
Grigory Alexandrov <alexaandrov@gmail.com>
2016-07-30 10:23:28 +0300

    + main controller

---
+++ app/controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\View;
+
+class Controller
+{
+    protected $view;
+
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
+    public function action($action)
+    {
+        $methodName = 'action' . $action;
+        return $this->$methodName();
+    }
+}
