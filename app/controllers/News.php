009934c2513f013f5138f70111ebf8306f343656
Grigory Alexandrov <alexaandrov@gmail.com>
2016-08-01 13:52:30 +0300

    Refactoring News controller

--- app/controllers/News.php
+++ app/controllers/News.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use \App\Models\News;
+
 class News extends Controller
 {
     protected $viewPath = __DIR__ . '/../views/news/';
@@ -9,7 +11,7 @@ class News extends Controller
     protected function actionIndex()
     {
         $this->view->title = 'News';
-        $this->view->news = \App\Models\News::FindAll();
+        $this->view->news = News::FindAll();
         $this->view->display($this->viewPath . 'index.php');
     }
 
@@ -17,31 +19,31 @@ class News extends Controller
     {
         $id = $_GET['id'];
         $this->view->title = 'News ' . $id;
-        $this->view->article = \App\Models\News::FindById($id);
+        $this->view->article = News::FindById($id);
         $this->view->display($this->viewPath . 'view.php');
     }
 
     protected function actionUpdate()
     {
         $id = $_GET['id'];
-        $model = \App\Models\News::findById($id);
+        $model = News::findById($id);
         
         $this->view->title = 'Edit news ' . $id;
         if (empty($_POST)) {
-            $this->view->article = \App\Models\News::FindById($id);
+            $this->view->article = News::FindById($id);
             $this->view->display($this->viewPath . 'update.php');
         } else {
             $model->setTitle($_POST['title']);
             $model->setText($_POST['text']);
             $this->view->status = $model->update();
-            $this->view->article = \App\Models\News::FindById($id);
+            $this->view->article = News::FindById($id);
             $this->view->display($this->viewPath . 'update.php');
         }
     }
 
     protected function actionDelete()
     {
-        $this->view->status = \App\Models\News::deleteById($_GET['id']);
+        $this->view->status = News::deleteById($_GET['id']);
         $this->actionIndex();
     }
 }
