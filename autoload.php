c013a88e4689bcd54a257512b3cfd7a486ceca6b
Grigory Alexandrov <alexaandrov@gmail.com>
2016-08-02 09:12:35 +0300

    + spl_autoload_register

--- autoload.php
+++ autoload.php
@@ -1,6 +1,22 @@
 <?php
 
-function __autoload($class)
+function my_app_autoload($class)
 {
-    require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    $filename = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($filename)) {
+        include $filename;
+    }
 }
+
+// Добавление автозагрузок в очередь
+
+// Первый способ регистрации автозагрузки с помощью имени функции
+spl_autoload_register('my_app_autoload');
+
+// Второй способ неявной регистрации автозагрузки
+spl_autoload_register(function($class) {
+    $filename = __DIR__ . '/' . str_replace(['\\', 'App'], ['/', 'lib'], $class) . '.php';
+    if (file_exists($filename)) {
+        include $filename;
+    }
+});
